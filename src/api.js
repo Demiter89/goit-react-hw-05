7653548b8e18a4a6e6c84f559aeb935a87a9129f
Maksym Shevchuk <maksymshevchuk89@gmai.com>
2025-03-29 23:24:46 +0100

    add index file

--- src/api.js
+++ src/api.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 export default async function findMoviesAPI(value) {
     
-    const API_KEY = '65e63a43c258a7dd70aa0c13e1b1fe41';
+    const API_KEY = 'b28ce4003d83b7bcee0c9dd6c31484cb';
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${value}`;
     
     const options = {
         headers: {
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NWU2M2E0M2MyNThhN2RkNzBhYTBjMTNlMWIxZmU0MSIsIm5iZiI6MTc0MjkwMDIzMS43OTAwMDAyLCJzdWIiOiI2N2UyOGMwNzE2YTNjNWMyMjRmMDVlNDEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Gj5KKh_5hoQkblhe7TtEiniX_OhNq0TGx1HwRS_dyOs'
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMjhjZTQwMDNkODNiN2JjZWUwYzlkZDZjMzE0ODRjYiIsIm5iZiI6MTc0MzI2OTA0MC4zODUsInN1YiI6IjY3ZTgyY2IwYmQxZjk2ZjllZjYzNzNmNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qn6L4dkcZTYQvM4P7dtmd-bkXmoWHxpLe_Pq5ifIBtE'
         }
     };
     try {
